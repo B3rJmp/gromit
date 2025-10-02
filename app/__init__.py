b4a5b399539adabb3a17f6234f9019285b873dc9
B3rJmp <thanerstevens@gmail.com>
2025-10-01 18:47:42 -0600

    modified init

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,17 @@
 from flask import Flask
-from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-load_dotenv()
 db = SQLAlchemy()
+migrate = Migrate()  # define globally
 
 def create_app():
-
-    # Load environment variables from .env
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    Migrate(app, db)
+    migrate.init_app(app, db)  # initialize here
 
     from app.blueprints.simon import simon_bp
     from app.blueprints.wallace import wallace_bp
