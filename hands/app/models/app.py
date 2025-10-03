b4d61db50015f0a4620da76bd50eaa5fe18639e8
B3rJmp <thanerstevens@gmail.com>
2025-10-03 12:00:01 -0600

    added columns to app model

--- hands/app/models/app.py
+++ hands/app/models/app.py
@@ -3,6 +3,8 @@ from app import db
 class App(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(50), nullable=False)
+  token = db.Column(db.String(100), nullable=True)
+  deep_link = db.Column(db.Boolean, server_default=False)
 
   host_apps = db.relationship("HostApp", backref="app", lazy=True)
 
