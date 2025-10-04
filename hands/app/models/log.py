182a51141cadf68849fe6b1799c20f0f5f4f9574
B3rJmp <thanerstevens@gmail.com>
2025-10-03 18:25:58 -0600

    increased log description character limit

--- hands/app/models/log.py
+++ hands/app/models/log.py
@@ -4,7 +4,7 @@ class Log(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
   log_type_id = db.Column(db.Integer, db.ForeignKey('log_type.id'), nullable=False)
-  description = db.Column(db.String(50), nullable=False)
+  description = db.Column(db.String(100), nullable=False)
   timestamp = db.Column(db.DateTime, server_default=db.func.now())
 
   log_type = db.relationship("LogType", backref="log")
