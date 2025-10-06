7f44fc7858a64ac3442d95b3ea338857cda8da98
B3rJmp <thanerstevens@gmail.com>
2025-10-06 12:21:59 -0600

    added logging

--- hands/app/blueprints/light.py
+++ hands/app/blueprints/light.py
@@ -1,4 +1,5 @@
 from flask import abort, Blueprint, request, jsonify
+from app import db
 from app.controllers import Light
 from app.models import User, Host, Variable, Log
 
@@ -9,6 +10,7 @@ def handle_light(location, state, token):
   USER = User.query.filter_by(token = token).first()
   if not USER:
     abort(403)
+  db.session.add(Log(user_id=USER.id,log_type_id=1,description=f"{USER.name} initiated light switch"))
   if(location == 'all'):
     hosts = Host.query.filter_by(host_type_id = 3).all()
   else:
@@ -16,8 +18,11 @@ def handle_light(location, state, token):
   try:
     for host in hosts:
       control_light(host.ip_address, state)
+    db.session.add(Log(user_id=USER.id,log_type_id=3,description=f"{USER.name} switched {location} lights to {state}"))
     return f"{location} set to {state}"
   except Exception as e:
+    print(23, e)
+    db.session.add(Log(user_id=USER.id,log_type_id=2,description=f"{USER.name} failed to switched {location} lights to {state}"))
     return f"Error: {e}", 500
 
 def control_light(ip_address, state):
